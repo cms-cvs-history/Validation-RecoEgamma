5f404b3638ae4b4e9ffc1b71cd69e01fc766bd23
mabalazs <sha1-814c98c95c0331e45c899668854de808bc93186d@cern.ch>
2007-02-22 20:22:56 +0000

    Updated to include DQM Support. Oversomes the lake of TH1D support of DQM which cause problem with FitSlicesY.
    Michael A. Balazs UVa. 2-22-07

--- src/SealModule.cc
+++ src/SealModule.cc
@@ -1,7 +1,10 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "Validation/RecoEgamma/interface/EgammaObjects.h"
+#include "Validation/RecoEgamma/interface/EgammaObjectsDQMLoader.h"
+
 
 DEFINE_SEAL_MODULE();
 
 DEFINE_ANOTHER_FWK_MODULE(EgammaObjects);
+DEFINE_ANOTHER_FWK_MODULE(DQMReadFileExample);
