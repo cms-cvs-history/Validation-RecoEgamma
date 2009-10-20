c22292bfadd1237227e257d1001114c625faf87f
David Chamont <sha1-cc2cbd4cf572176bc632dcdb55d943ce51d4e6af@cern.ch>
2009-10-20 12:08:07 +0000

    *** empty log message ***

--- test/Step3Pt35Histos_new_cfg.py
+++
@@ -1,7 +0,0 @@
-
-import sys
-import os
-
-from Step3Pt35Histos_cfg import *
-
-process.dqmSaver.workflow = '/Pt35/CMSSW_'+os.environ['TEST_RELEASE']+'/RECO'
--- test/Step3QcdHistos_new_cfg.py
+++
@@ -1,7 +0,0 @@
-
-import sys
-import os
-
-from Step3QcdHistos_cfg import *
-
-process.dqmSaver.workflow = '/Qcd/CMSSW_'+os.environ['TEST_RELEASE']+'/RECO'
